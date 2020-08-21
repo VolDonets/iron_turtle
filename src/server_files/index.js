0bf684a06299793ed41e2f942563e86de5b04b1d
VolDonets <vovan.s.marsa@gmail.com>
2020-08-21 15:46:47 +0300

    Added and tested the keyboard events handle system.

--- src/server_files/index.js
+++ src/server_files/index.js
@@ -1,17 +1,48 @@
 var ws;
 
-$(function begin() {
-    $('#id_form_button_left').click(function onClick(e) {
+function button_handler(key_code) {
+    if (key_code === 'ArrowUp') {
+        ws.send("mvT_UP");
+    } else if (key_code === 'ArrowDown') {
+        ws.send("mvT_DOWN");
+    } else if (key_code === 'ArrowRight') {
+        ws.send("mvT_RIGHT");
+    } else if (key_code === 'ArrowLeft') {
         ws.send("mvT_LEFT");
+    } else if (key_code === 'q' || key_code === 'Q') {
+        ws.send("zmC_MINUS")
+    } else if (key_code === 'e' || key_code === 'E') {
+        ws.send("zmC_PLUS");
+    } else if (key_code === 'w' || key_code === 'W') {
+        ws.send("mvC_UP");
+    } else if (key_code === 's' || key_code === 'S') {
+        ws.send("mvC_DOWN");
+    } else if (key_code === 'a' || key_code === 'A') {
+        ws.send("mvC_LEFT");
+    } else if (key_code === 'd' || key_code === 'D') {
+        ws.send("mvC_RIGHT");
+    }
+}
+
+$(function begin() {
+    document.addEventListener('keydown', (e) => {
+        if (!e.repeat)
+            button_handler(e.key);
+        else
+            button_handler(e.key);
+    });
+
+    $('#id_form_button_left').click(function onClick(e) {
+        //ws.send("mvT_LEFT");
     });
     $('#id_form_button_right').click(function onClick(e) {
-        ws.send("mvT_RIGHT");
+        //ws.send("mvT_RIGHT");
     });
     $('#id_form_button_up').click(function onClick(e) {
-        ws.send("mvT_UP");
+        //ws.send("mvT_UP");
     });
     $('#id_form_button_down').click(function onClick(e) {
-        ws.send("mvT_DOWN");
+        //ws.send("mvT_DOWN");
     });
 
     ws = new WebSocket('ws://' + document.location.host + '/chart', ['string', 'foo']);
