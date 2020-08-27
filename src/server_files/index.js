50d8405203efba3f3a4d2bd6d731e2617ef773ea
VolDonets <vovan.s.marsa@gmail.com>
2020-08-27 16:08:25 +0300

    Added a possibility to handle a multiple keyboard buttons pushing. !Danger! Here I had a bug with last pushed button, if other buttons are still pushing, and you push new button, when you stop pushing it, the handling the first button/buttons stops (possibly solution here is just use a loop with function sleep for waiting and not overloading network/browser).

--- src/server_files/index.js
+++ src/server_files/index.js
@@ -70,36 +70,178 @@ function playStream(videoElement, configuration, reportErrorCB) {
     ws.addEventListener("message", onServerMessage);
 }
 
-function button_handler(key_code) {
-    if (key_code === 'ArrowUp') {
-        ws.send("mvT_UP");
-    } else if (key_code === 'ArrowDown') {
-        ws.send("mvT_DOWN");
-    } else if (key_code === 'ArrowRight') {
-        ws.send("mvT_RIGHT");
-    } else if (key_code === 'ArrowLeft') {
-        ws.send("mvT_LEFT");
-    } else if (key_code === 'q' || key_code === 'Q') {
-        ws.send("zmC_MINUS")
-    } else if (key_code === 'e' || key_code === 'E') {
-        ws.send("zmC_PLUS");
-    } else if (key_code === 'w' || key_code === 'W') {
-        ws.send("mvC_UP");
-    } else if (key_code === 's' || key_code === 'S') {
-        ws.send("mvC_DOWN");
-    } else if (key_code === 'a' || key_code === 'A') {
-        ws.send("mvC_LEFT");
-    } else if (key_code === 'd' || key_code === 'D') {
-        ws.send("mvC_RIGHT");
+
+var isQ_ButtonPushed = false;
+var isE_ButtonPushed = false;
+var isW_ButtonPushed = false;
+var isA_ButtonPushed = false;
+var isS_ButtonPushed = false;
+var isD_ButtonPushed = false;
+
+var isUP_ButtonPushed = false;
+var isDOWN_ButtonPushed = false;
+var isRIGHT_ButtonPushed = false;
+var isLEFT_ButtonPushed = false;
+
+var isANY_ButtonPushed = false;
+
+
+function set_keydown(key_code) {
+    if (key_code === 'ArrowUp')
+        isUP_ButtonPushed = true;
+    else if (key_code === 'ArrowDown')
+        isDOWN_ButtonPushed = true;
+    else if (key_code === 'ArrowRight')
+        isRIGHT_ButtonPushed = true;
+    else if (key_code === 'ArrowLeft')
+        isLEFT_ButtonPushed = true;
+    else if (key_code === 'q' || key_code === 'Q')
+        isQ_ButtonPushed = true;
+    else if (key_code === 'e' || key_code === 'E')
+        isE_ButtonPushed = true;
+    else if (key_code === 'w' || key_code === 'W')
+        isW_ButtonPushed = true;
+    else if (key_code === 's' || key_code === 'S')
+        isS_ButtonPushed = true;
+    else if (key_code === 'a' || key_code === 'A')
+        isA_ButtonPushed = true;
+    else if (key_code === 'd' || key_code === 'D')
+        isD_ButtonPushed = true;
+
+    if (isQ_ButtonPushed || isW_ButtonPushed || isE_ButtonPushed
+        || isA_ButtonPushed || isS_ButtonPushed || isD_ButtonPushed
+        || isUP_ButtonPushed || isDOWN_ButtonPushed || isLEFT_ButtonPushed || isRIGHT_ButtonPushed)
+        isANY_ButtonPushed = true;
+}
+
+function set_keyup(key_code) {
+    if (key_code === 'ArrowUp')
+        isUP_ButtonPushed = false;
+    else if (key_code === 'ArrowDown')
+        isDOWN_ButtonPushed = false;
+    else if (key_code === 'ArrowRight')
+        isRIGHT_ButtonPushed = false;
+    else if (key_code === 'ArrowLeft')
+        isLEFT_ButtonPushed = false;
+    else if (key_code === 'q' || key_code === 'Q')
+        isQ_ButtonPushed = false;
+    else if (key_code === 'e' || key_code === 'E')
+        isE_ButtonPushed = false;
+    else if (key_code === 'w' || key_code === 'W')
+        isW_ButtonPushed = false;
+    else if (key_code === 's' || key_code === 'S')
+        isS_ButtonPushed = false;
+    else if (key_code === 'a' || key_code === 'A')
+        isA_ButtonPushed = false;
+    else if (key_code === 'd' || key_code === 'D')
+        isD_ButtonPushed = false;
+
+    if (!(isQ_ButtonPushed || isW_ButtonPushed || isE_ButtonPushed
+        || isA_ButtonPushed || isS_ButtonPushed || isD_ButtonPushed
+        || isUP_ButtonPushed || isDOWN_ButtonPushed || isLEFT_ButtonPushed || isRIGHT_ButtonPushed))
+        isANY_ButtonPushed = false;
+}
+
+function send_control_message(){
+    if(isANY_ButtonPushed){
+        let json_str = "{\"comm\":["
+        let isFirst = true;
+
+        if(isQ_ButtonPushed) {
+            ws.send("zmC_PLUS");
+            json_str += "\"ZCP\"";
+            isFirst = false;
+        }
+        if(isE_ButtonPushed) {
+            ws.send("zmC_MINUS");
+            if (isFirst) {
+                json_str += "\"ZCM\"";
+                isFirst = false;
+            } else
+                json_str += ",\"ZCM\"";
+        }
+        if(isW_ButtonPushed) {
+            ws.send("mvC_UP");
+            if (isFirst) {
+                json_str += "\"MCU\"";
+                isFirst = false;
+            } else
+                json_str += ",\"MCU\"";
+        }
+        if(isS_ButtonPushed) {
+            ws.send("mvC_DOWN");
+            if (isFirst) {
+                json_str += "\"MCD\"";
+                isFirst = false;
+            } else
+                json_str += ",\"MCD\"";
+
+        }
+        if(isA_ButtonPushed) {
+            ws.send("mvC_LEFT");
+            if (isFirst) {
+                json_str += "\"MCL\"";
+                isFirst = false;
+            } else
+                json_str += ",\"MCL\"";
+        }
+        if(isD_ButtonPushed) {
+            ws.send("mvC_RIGHT");
+            if (isFirst) {
+                json_str += "\"MCR\"";
+                isFirst = false;
+            } else
+                json_str += ",\"MCR\"";
+        }
+
+        if(isUP_ButtonPushed) {
+            ws.send("mvT_UP");
+            if (isFirst) {
+                json_str += "\"MTU\"";
+                isFirst = false;
+            } else
+                json_str += ",\"MTU\"";
+        }
+        if(isDOWN_ButtonPushed) {
+            ws.send("mvT_DOWN");
+            if (isFirst) {
+                json_str += "\"MTD\"";
+                isFirst = false;
+            } else
+                json_str += ",\"MTD\"";
+        }
+        if(isRIGHT_ButtonPushed) {
+            ws.send("mvT_RIGHT");
+            if (isFirst) {
+                json_str += "\"MTR\"";
+                isFirst = false;
+            } else
+                json_str += ",\"MTR\"";
+        }
+        if(isLEFT_ButtonPushed) {
+            ws.send("mvT_LEFT");
+            if (isFirst)
+                json_str += "\"MTL\"";
+            else
+                json_str += ",\"MTL\"";
+        }
+        json_str += "]}";
+        console.log(json_str);
     }
 }
 
 $(function begin() {
     document.addEventListener('keydown', (e) => {
-        if (!e.repeat)
-            button_handler(e.key);
-        else
-            button_handler(e.key);
+        if (!e.repeat) {
+            set_keydown(e.key);
+            send_control_message();
+        } else {
+            send_control_message();
+        }
+    });
+
+    document.addEventListener('keyup', (e) => {
+       set_keyup(e.key);
     });
 
     $('#id_form_button_left').click(function onClick(e) {
