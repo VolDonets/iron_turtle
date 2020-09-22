1f9f9ac9c7f2e647a9db0de4f0cb832cbb5049a8
VolDonets <vovan.s.marsa@gmail.com>
2020-09-22 14:18:02 +0300

    Added a few documentation comments to the class interface.

--- process_camera/form_detector_ocv/form_detection_processor.h
+++ process_camera/form_detector_ocv/form_detection_processor.h
@@ -5,7 +5,7 @@
 #ifndef REAR_SIGHT_CLONE_PROJECT_FORM_DETECTION_PROCESSOR_H
 #define REAR_SIGHT_CLONE_PROJECT_FORM_DETECTION_PROCESSOR_H
 
-
+//just pathes
 //#define MODEL_PATH                          "src/dnn/yolov3_tiny/yolov3-tiny.cfg"
 //#define WIDTH_PATH                          "src/dnn/yolov3_tiny/yolov3-tiny.weights"
 //#define COCO_NAMES_PATH                     "src/dnn/yolov3_tiny/coco.names"
@@ -37,22 +37,42 @@
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core/utils/trace.hpp>
+#include <opencv2/tracking.hpp>
 
+#include <dlib/image_processing.h>
+#include <dlib/image_keypoint.h>
+#include <dlib/svm_threaded.h>
+#include <dlib/opencv.h>
+
+/**class FormDetectionProcessor - is a class which realize functionality for an image detection and an image tracking,
+ * (depending from the realization). The constructor initialize needed mutex, and start detection thread*/
 class FormDetectionProcessor {
 private:
+    /**this list contains a Mat's queue which waiting for processing (it can be an image detection or an image tracking (depending from the realization)*/
     std::list<cv::Mat> queueFrames;
+    /**this array contains a list of the last detected object rectangles*/
     std::vector<cv::Rect> *formsCoords = nullptr;
+    /**this thread is a image processing thread, here processes an object detection and an object tracking*/
     std::thread recognitionProcessThread;
 
+    /**this mutex synchronize a block code where we process a resources (here is - std::list<cv::Mat> queueFrames)*/
     std::mutex mutexRes;
+    /**this mutex used for blocking processing, it blocks code when we trying to process image, but actually we don't have a Mat object for this
+     * (it means std::list<cv::Mat> queueFrames is empty)
+     * Firstly it blocks when we are creating a FromDetectionProcessor class (calls in constructor)*/
     std::mutex mutexProc;
 
 public:
+    /**this is a default constructor here in other thread blocks mutesProc, and also starts a new thread for the independent object processing*/
     FormDetectionProcessor();
+    /**this is a default destructor, this one doing nothing, cause in this class used smart pointers (std:: and cv:: memory allocation and de-allocation)*/
     ~FormDetectionProcessor();
 
+    /**this function just add a new Mat object to the queueFrames for the next processing*/
     void add_frame(cv::Mat frame);
+    /**this function starts a new thread for image processing*/
     void processRecognition();
+    /**this function returns an empty std::vector<cv::Rect> or nullptr if it was nothing to detect, or an filled std::vector<... if it was detect some objects*/
     std::vector<cv::Rect> *getLastDetectedFaces();
 };
 
