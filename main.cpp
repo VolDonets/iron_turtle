fc15701fb3541fba5e491c35a3fbef81490906f6
VolDonets <vovan.s.marsa@gmail.com>
2020-10-01 15:22:38 +0300

    Added the '^C' signal handler (possibly it should solve the problem with the program freezing, when client push '^C', but it doesn't).

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
+#include <signal.h>
 
 /// Included a source code for working with Seasocks server.
 #include "web_server_worker.h"
 
+void my_handler(int s){
+    printf("Caught signal %d\n",s);
+    exit(1);
+}
+
 /** This is a start point of the program*/
 int main() {
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
     //this is an instance of the interrupt handling delegator.
     DelegateWS* delegate = DelegateWS::getInstance();
     //this is start point of working webserver and system at all, also here starts threads for webserver, handling thread,
