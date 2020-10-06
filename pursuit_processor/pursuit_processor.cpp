b56a93c98878fc22b16f81fa3c8b71281919d606
VolDonets <vovan.s.marsa@gmail.com>
2020-10-06 20:36:56 +0300

    Add a few function for the pursuit processor.

--- pursuit_processor/pursuit_processor.cpp
+++ pursuit_processor/pursuit_processor.cpp
@@ -16,6 +16,23 @@ PursuitProcessor::~PursuitProcessor() {
 
 }
 
+int PursuitProcessor::x_offset(const cv::Rect& newPosition) {
+    // here we take a new position by x and plus a half of the object width (this means
+    // we calculate an object position, as absolute value), than from it we minus
+    // old position by x and minus a half of the old object width
+    return newPosition.x + (newPosition.width / 2) - fixedRectangleCoord.x - (fixedRectangleCoord.width / 2);
+}
+
+double PursuitProcessor::y_delta_moving(const cv::Rect& newPosition) {
+    // Here we calculate a new object area and we calculate an old object area,
+    // than using a ration between area and roi's areas we can get a distance between
+    // the old object and the new object
+    double Sroi_0, Sroi_1;
+    Sroi_0 = fixedRectangleCoord.width * fixedRectangleCoord.height;
+    Sroi_1 = newPosition.width * newPosition.height;
+    return START_DISTANCE_TO_AN_OBJECT * ((Sroi_1 / Sroi_0) - 1);
+}
+
 void PursuitProcessor::update_current_speed_params() {
 
 }
--- pursuit_processor/pursuit_processor.h
+++ pursuit_processor/pursuit_processor.h
@@ -16,9 +16,12 @@
  *         that means we should to stop sending a commands.*/
 constexpr int SERVER_WAIT_STEPS = 10;
 /// @brief this constant is a code of the successful operation
-constexpr int SUCCESSFUL_OPERATION   = 0;
+constexpr int SUCCESSFUL_OPERATION = 0;
 /// @brief this constant is s code of the unsuccessful operation - the reason is problems with connection to the serial devise.
 constexpr int SERIAL_MANAGER_PROBLEM = -1;
+
+/// @brief this is constant is a distance in mm from camera to the object
+constexpr double START_DISTANCE_TO_AN_OBJECT = 1000;  // TODO. I should measure it soon.
 
 /** @brief This is class for the processing auto moving of the iron turtle.
  *  This has a start rectangle size and place and recalculate moving direction and speed by a new object rectangle and position.
@@ -33,25 +36,47 @@ private:
 
     /// @brief this function recalculate the current speed values for the right and the left wheels as wanted to achieve in the wanted values
     void update_current_speed_params();
+
     /// @brief this function sets a new value for the field wantedLeftWheelSpeed (INCREASE if possible)
     void move_faster_left_wheel();
+
     /// @brief this function sets a new value for the field wantedLeftWheelSpeed (DECREASE if possible)
     void move_slower_left_wheel();
+
     /// @brief this function sets a new value for the field wantedRightWheelSpeed (INCREASE if possible)
     void move_fasted_right_wheel();
+
     /// @brief this function sets a new value for the field wantedRightWheelSpeed (DECREASE if possible)
     void move_slower_right_wheel();
+
     /// @brief this function starts new thread for the wheel's speed processing and for the sending control commands to the turtle engine controller
     void process_pursuit();
+
+    /**
+     * @brief this function calculate an object position bias
+     * @param newPosition - a cv rectangle with a new object position
+     * @return current object bias (if haven't image it returns 0)
+     */
+    int x_offset(const cv::Rect &newPosition);
+
+    /**
+     * @brief this function calculate an distance between an original object position and a new one.
+     * @param newPosition - a cv rectangle with a new object position
+     * @return distance between an original object position and a new one.
+     */
+    double y_delta_moving(const cv::Rect &newPosition);
+
 public:
     /** @brief this is a default constructor, here inits all needed fields
      *  @warning this constructor doesn't start new thread so you should manually start it, for activating the pursuit function*/
     PursuitProcessor();
+
     /** @brief this is a default constructor, here inits NOT all needed fields
      *  @param serialManager - this is a driver wrapper for the working with serial device (here it's connected by UARD
      *  the iron turtle engines controller
      *  @warning this constructor doesn't start new thread so you should manually start it, for activating the pursuit function*/
     PursuitProcessor(std::shared_ptr<SerialManager> serialManager);
+
     /** @brief this is a default destructor
      *  @warning this one doing nothing */
     ~PursuitProcessor();
@@ -60,6 +85,7 @@ public:
     /** @brief this function returns current speed
      *  @return returns current speed */
     int get_speed();
+
     /** @brief this function returns current battery voltage
      * @return returns current battery voltage of the iron turtle battery*/
     int get_battery_voltage();
@@ -67,9 +93,11 @@ public:
     /** @brief This function returns code of operation for the STOPPING thread and it trying if possible to stop the processing thread.
      *  @return an operation result code */
     int stop_processing_thread();
+
     /** @brief This function returns code of operation for the RESTARTING thread and it trying if possible to stop the processing thread.
      *  @return an operation result code */
     int restart_processing_thread(cv::Rect fixedRectangleCoord);
+
     /** @brief This function returns status of the processing thread. If a thread works - the function returns 'true', otherwise it returns - 'false'.
      *  @return an operation result code */
     bool is_process_moving();
@@ -80,6 +108,7 @@ public:
 
     /// @brief a server call this function, when it has active connection with an active client
     void say_server_here();
+
     /// @brief a server call this function, when it lost an active client
     void say_server_leave();
 };
