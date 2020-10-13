f91f19883b83cb3daaf50d7ba07e1c10e6bf43a1
VolDonets <vovan.s.marsa@gmail.com>
2020-10-13 12:30:08 +0300

    Added a few comments and writen a bit code lines for rotation processing and moving processing in accordance with a current position and an aim's position.

--- pursuit_processor/pursuit_processor.cpp
+++ pursuit_processor/pursuit_processor.cpp
@@ -44,31 +44,46 @@ double PursuitProcessor::y_delta_moving(const cv::Rect& newPosition) {
 }
 
 void PursuitProcessor::process_pursuit() {
+    // Starting an independent thread for calculating a new position for the iron turtle
+    // in accordance with a current position and an aim's position.
     this->movingProcessingThread = std::thread([this]() {
+        // this is an offset by X-axis, it defines how big angle should rotate the iron turtle body
         double xOffset = 0.0;
+        // this is an offset by Y-axis, it defines how big distance between an object and the iron turtle body
         double yOffset = 0.0;
+        // this is an angle for rotation
         double phiOffset = 0.0;
 
+        // this variable defines a speed changing, it uses for smooth moving
         int currentPower = 0;
+        // this variable contains current speed for the left wheel in percent from the maximum speed
         double leftWheelSpeed = 0.0;
+        // this variable contains current speed for the right wheel in percent from the maximum speed
         double rightWheelSpeed = 0.0;
+        // this variable uses for minimazing jerks, for sending zero speed status
         int sendZeroSpeedTimes = 0;
+        // this constant uses for setting the maximum count of sending zero speed status
         constexpr int MAX_ZERO_SPEED_SEND_TIMES = 5;
 
+        // the 'infinite' loop for processing the iron turtle moving in according with a current position and an aim's position.
         while (isProcessThread) {
+            // means if server has a client connection, so we should process moving
             if (serverCounter > 0) {
+                // if we has a rectangle of detected object
                 if (!newRectangleCoordsList.empty()) {
                     cv::Rect newRectPos = newRectangleCoordsList.front();
                     newRectangleCoordsList.pop_front();
                     xOffset = x_offset(newRectPos);
-                    yOffset = y_delta_moving(newRectPos);
+                    yOffset = y_delta_moving(newRectPos) - START_DISTANCE_TO_AN_OBJECT;
                     phiOffset = xOffset / PIXELS_PER_ANGLE_RPI_CAM_V2;
                 }
-                if (abs(xOffset) >= 0) {
+                if (abs(xOffset) > 0) {
                     double lRotation = abs(phiOffset) * SMALL_ROTATION_RADIUS;
 
                     int accelerationSteps = 0;
                     int continuingSteps = 0;
+                    // this code block calculates acceleration steps (when speed increase and decrease),
+                    // and continuing steps (when speed has the same value whole time)
                     if (MAX_ACCELERATION_DISTANCE_MM > (lRotation / 2)) {
                         accelerationSteps = (sqrt(lRotation / MAX_ACHIEVED_ACCELERATION_MM) / TIME_INTERVAL_BETWEEN_SENDING_COMMANDS);
                     } else {
@@ -76,33 +91,85 @@ void PursuitProcessor::process_pursuit() {
                         continuingSteps = ((lRotation - (2 * MAX_ACCELERATION_DISTANCE_MM)) / MAX_ACHIEVED_SPEED_MM) / TIME_INTERVAL_BETWEEN_SENDING_COMMANDS;
                     }
 
-                    for (int inx = 0; inx < 2; inx++) {
+                    sendZeroSpeedTimes = 0;
+
+                    // ******************************************************************************************* //
+                    //                                  ROTATION STAGE
+                    // make wheel controller calm down
+                    for (int inx = 0; inx < 3; inx++) {
                         ironTurtleAPI->sendSpeedData(0.0, 0.0, 0, 0, PROTOCOL_SOM_NOACK);
                         std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
                     }
 
+                    // linearly increase wheels speed
                     for (int inx = 0; inx < accelerationSteps; inx++) {
-                        leftWheelSpeed += MAX_ACHIEVED_ACCELERATION_PERCENT;
-                        rightWheelSpeed += MAX_ACHIEVED_ACCELERATION_PERCENT;
+                        leftWheelSpeed = (xOffset > 0) ? (leftWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT) : (leftWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT);
+                        rightWheelSpeed = (xOffset > 0) ? (rightWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT) : (rightWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT);
                         currentPower = (currentPower < 200) ? (currentPower + 50) : 200;
                         ironTurtleAPI->sendSpeedData(leftWheelSpeed, rightWheelSpeed, currentPower, 5, PROTOCOL_SOM_NOACK);
                         std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
                     }
+
+                    // continuing a current speed value
                     for (int inx = 0; inx < continuingSteps; inx++) {
                         ironTurtleAPI->sendSpeedData(MAX_ACHIEVED_SPEED_PERCENT, MAX_ACHIEVED_SPEED_PERCENT, 200, 5, PROTOCOL_SOM_NOACK);
                         std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
                     }
+
+                    // linearly decrease wheels speed
                     for (int inx = 0; inx < accelerationSteps; inx++) {
-                        leftWheelSpeed -= MAX_ACHIEVED_ACCELERATION_PERCENT;
-                        rightWheelSpeed -= MAX_ACHIEVED_ACCELERATION_PERCENT;
+                        leftWheelSpeed = (xOffset > 0) ? (leftWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT) : (leftWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT);
+                        rightWheelSpeed = (xOffset > 0) ? (rightWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT) : (rightWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT);
                         currentPower = (currentPower < 200) ? (currentPower + 50) : 200;
                         ironTurtleAPI->sendSpeedData(leftWheelSpeed, rightWheelSpeed, currentPower, 5, PROTOCOL_SOM_NOACK);
                         std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
-                        sendZeroSpeedTimes = 0;
+                    }
+                    // ******************************************************************************************* //
+
+                    // this code block calculates acceleration steps (when speed increase and decrease),
+                    // and continuing steps (when speed has the same value whole time)
+                    if (MAX_ACCELERATION_DISTANCE_MM > (lRotation / 2)) {
+                        accelerationSteps = (sqrt(abs(yOffset) / MAX_ACHIEVED_ACCELERATION_MM) / TIME_INTERVAL_BETWEEN_SENDING_COMMANDS);
+                    } else {
+                        accelerationSteps = (sqrt(MAX_ACCELERATION_DISTANCE_MM / MAX_ACHIEVED_ACCELERATION_MM) / TIME_INTERVAL_BETWEEN_SENDING_COMMANDS);
+                        continuingSteps = ((abs(yOffset) - (2 * MAX_ACCELERATION_DISTANCE_MM)) / MAX_ACHIEVED_SPEED_MM) / TIME_INTERVAL_BETWEEN_SENDING_COMMANDS;
                     }
 
+                    // ******************************************************************************************* //
+                    //                                  MOVING TO THE AIM STAGE
+                    // make wheel controller calm down
+                    for (int inx = 0; inx < 3; inx++) {
+                        ironTurtleAPI->sendSpeedData(0.0, 0.0, 0, 0, PROTOCOL_SOM_NOACK);
+                        std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
+                    }
+
+                    // linearly increase wheels speed
+                    for (int inx = 0; inx < accelerationSteps; inx++) {
+                        leftWheelSpeed = (yOffset > 0) ? (leftWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT) : (leftWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT);
+                        rightWheelSpeed = (yOffset > 0) ? (rightWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT) : (rightWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT);
+                        currentPower = (currentPower < 200) ? (currentPower + 50) : 200;
+                        ironTurtleAPI->sendSpeedData(leftWheelSpeed, rightWheelSpeed, currentPower, 5, PROTOCOL_SOM_NOACK);
+                        std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
+                    }
+
+                    // continuing a current speed value
+                    for (int inx = 0; inx < continuingSteps; inx++) {
+                        ironTurtleAPI->sendSpeedData(MAX_ACHIEVED_SPEED_PERCENT, MAX_ACHIEVED_SPEED_PERCENT, 200, 5, PROTOCOL_SOM_NOACK);
+                        std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
+                    }
+
+                    // linearly decrease wheels speed
+                    for (int inx = 0; inx < accelerationSteps; inx++) {
+                        leftWheelSpeed = (yOffset > 0) ? (leftWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT) : (leftWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT);
+                        rightWheelSpeed = (yOffset > 0) ? (rightWheelSpeed - MAX_ACHIEVED_ACCELERATION_PERCENT) : (rightWheelSpeed + MAX_ACHIEVED_ACCELERATION_PERCENT);
+                        currentPower = (currentPower < 200) ? (currentPower + 50) : 200;
+                        ironTurtleAPI->sendSpeedData(leftWheelSpeed, rightWheelSpeed, currentPower, 5, PROTOCOL_SOM_NOACK);
+                        std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_THREAD_TIME_MS));
+                    }
+                    // ******************************************************************************************* //
+
 #ifdef MY_PURSUIT_TESTING
-                    std::string message = "Dx: " + std::to_string((int)xOffset)
+                    std::string message = "Dy: " + std::to_string((int)yOffset)
                             + ", accSt: " + std::to_string(accelerationSteps)
                             + ", contSt: " + std::to_string(continuingSteps);
 
